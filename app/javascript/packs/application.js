00182fae617d66be50f489a41c20bd31c2e47667
Brandon Keepers <brandon@opensoul.org>
2021-10-13 07:58:49 -0700

    Rename client-side service worker setup

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "service_worker"
+import "register_service_worker"
 
 Rails.start()
 Turbolinks.start()
