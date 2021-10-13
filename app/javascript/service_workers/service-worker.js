7582633752e66857ed29d1dd6bf879551a12e38a
Brandon Keepers <brandon@opensoul.org>
2021-10-13 07:31:39 -0700

    Refactor service worker

--- app/javascript/service_workers/service-worker.js
+++ app/javascript/service_workers/service-worker.js
@@ -1,61 +1,55 @@
 /* eslint-env serviceworker */
 
-const CACHE_VERSION = 3
+const CACHE_VERSION = 4
 const CACHES = {
   prefetch: 'prefetch-cache-v' + CACHE_VERSION,
   readthrough: 'read-through-cache-v' + CACHE_VERSION
 }
 
-self.addEventListener('install', function (event) {
+self.addEventListener('install', async event => {
   event.waitUntil(
-    caches.open(CACHES.prefetch).then(async (cache) => {
+    caches.open(CACHES.prefetch).then(async cache => {
       const urlsToPrefetch = await fetch('/offline.json').then(response => response.json())
 
-      const cachePromises = urlsToPrefetch.map(function (urlToPrefetch) {
+      await Promise.all(urlsToPrefetch.map(async urlToPrefetch => {
         const url = new URL(urlToPrefetch, location.href)
         const request = new Request(url, { mode: 'no-cors' })
 
-        return fetch(request).then(function (response) {
-          if (response.status >= 400) {
-            throw new Error(`request for ${urlToPrefetch} failed:`, response.statusText)
-          }
+        const response = await fetch(request)
 
-          return cache.put(request, response)
-        }).catch(function (error) {
-          console.error('Not caching ' + urlToPrefetch + ' due to ' + error)
-        })
-      })
+        if (response.status >= 400) {
+          throw new Error(`request for ${urlToPrefetch} failed:`, response.statusText)
+        }
 
-      return Promise.all(cachePromises).then(function () {
-        console.log('Pre-fetching complete.')
-      })
+        cache.put(request, response)
+      }))
+
+      console.log('Pre-fetching complete.')
     }).catch(function (error) {
       console.error('Pre-fetching failed:', error)
     })
   )
 })
 
-self.addEventListener('activate', function (event) {
-  // Delete all old caches
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (!Object.values(CACHES).includes(cacheName)) {
-            console.log('Deleting out of date cache:', cacheName)
-            return caches.delete(cacheName)
-          }
+self.addEventListener('activate', event => {
+  clients.claim()
 
-          return Promise.resolve()
-        })
-      )
-    })
-  )
+  // Delete all old caches
+  event.waitUntil(caches.keys().then(async cacheNames => {
+    return Promise.all(cacheNames.map(cacheName => {
+      if (!Object.values(CACHES).includes(cacheName)) {
+        console.log('Deleting out of date cache:', cacheName)
+        return caches.delete(cacheName)
+      }
+
+      return Promise.resolve()
+    }))
+  }))
 })
 
-self.addEventListener('fetch', function (event) {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request.url).then(function (response) {
+    caches.match(event.request.url).then(async response => {
       // Return cached response
       if (response) {
         console.log('Found response in cache:', response)
@@ -63,14 +57,13 @@ self.addEventListener('fetch', function (event) {
       }
 
       // No response found in cache, fetch from network
-      return fetch(event.request).then(function (response) {
+      return fetch(event.request).then(async response => {
         if (response.status < 400 && event.request.type === 'basic') {
           console.log('Caching response from network:', response)
 
           // Cache response
-          caches.open(CACHES.readthrough).then(function (cache) {
-            return cache.put(event.request, response.clone())
-          })
+          const cache = await caches.open(CACHES.readthrough)
+          cache.put(event.request, response.clone())
         }
 
         return response
