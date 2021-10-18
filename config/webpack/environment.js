06c214236a65ae77042966b35a001d55a332eec4
Brandon Keepers <brandon@opensoul.org>
2021-10-18 08:30:23 -0700

    $  bundle exec rails webpacker:install:vue

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,10 @@
 const { config, environment } = require('@rails/webpacker');
+const { VueLoaderPlugin } = require('vue-loader')
+const vue = require('./loaders/vue')
 
 const WebpackerPwa = require('webpacker-pwa')
 new WebpackerPwa(config, environment)
 
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
 module.exports = environment
