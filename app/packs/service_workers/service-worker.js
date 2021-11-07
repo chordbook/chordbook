a8677f58ec8306f472896e62317b71ab1cbda692
Brandon Keepers <brandon@opensoul.org>
2021-11-06 20:24:34 -0700

    Serve js/css from cache, cache documents for offline

--- app/packs/service_workers/service-worker.js
+++ app/packs/service_workers/service-worker.js
@@ -1,5 +1,24 @@
 /* eslint-env serviceworker */
 import { precacheAndRoute } from 'workbox-precaching'
+import { registerRoute } from 'workbox-routing';
+import { CacheFirst, NetworkFirst } from 'workbox-strategies';
+import { ExpirationPlugin } from 'workbox-expiration';
+
+registerRoute(
+  ({request}) => request.destination === 'script' || request.destination === 'style',
+  new CacheFirst()
+);
+
+registerRoute(
+  ({url, request}) => url.origin === self.location.origin && request.destination === 'document',
+  new NetworkFirst({
+    networkTimeoutSeconds: 3,
+    cacheName: 'documents',
+    plugins: [
+      new ExpirationPlugin({ maxEntries: 500 }),
+    ],
+  })
+);
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') self.skipWaiting()
