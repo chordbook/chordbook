858a4f4530299463bda5158fd5df37d12a54bd07
Brandon Keepers <brandon@opensoul.org>
2021-11-06 20:48:19 -0700

    Update styles for PWA banner

--- app/packs/components/reload-pwa.vue
+++ app/packs/components/reload-pwa.vue
@@ -14,50 +14,21 @@ const close = async() => {
 </script>
 
 <template>
-  <div
-      v-if="offlineReady || needRefresh"
-      class="pwa-toast"
-      role="alert"
-  >
-    <div class="message">
-      <span v-if="offlineReady">
-        App ready to work offline
-      </span>
+ <div v-if="offlineReady || needRefresh" role="alert"
+    class="flex flex-wrap md:flex-nowrap bg-blue-800 text-white text-sm px-6 py-2 justify-between align-middle">
+    <div class="message mt-1">
+      <span v-if="offlineReady"> App ready to work offline</span>
       <span v-else>
         New content available, click on reload button to update.
       </span>
     </div>
-    <button v-if="needRefresh" @click="updateServiceWorker()">
-      Reload
-    </button>
-    <button @click="close">
-      Close
-    </button>
+    <div class="buttons flex align-middle mt-2 md:mt-0">
+      <button v-if="needRefresh" @click="updateServiceWorker()" class="btn btn-small btn-muted mr-4">
+        Reload
+      </button>
+      <button @click="close" class="btn btn-small btn-muted">
+        Close
+      </button>
+    </div>
   </div>
 </template>
-
-<style>
-.pwa-toast {
-  position: fixed;
-  right: 0;
-  bottom: 0;
-  margin: 16px;
-  padding: 12px;
-  border: 1px solid #8885;
-  border-radius: 4px;
-  z-index: 1;
-  text-align: left;
-  box-shadow: 3px 4px 5px 0 #8885;
-  background-color: white;
-}
-.pwa-toast .message {
-  margin-bottom: 8px;
-}
-.pwa-toast button {
-  border: 1px solid #8885;
-  outline: none;
-  margin-right: 5px;
-  border-radius: 2px;
-  padding: 3px 10px;
-}
-</style>
