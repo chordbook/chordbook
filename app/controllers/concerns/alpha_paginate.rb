b62eb0578549038b5cdcd26694a03452745454aa
Brandon Keepers <brandon@opensoul.org>
2021-12-13 22:16:11 -0500

    Fix alpha pagination for numbers

--- app/controllers/concerns/alpha_paginate.rb
+++ app/controllers/concerns/alpha_paginate.rb
@@ -5,8 +5,11 @@ module AlphaPaginate
 
   included do
     scope :starts_with, ->(key, letter) {
-      where(arel_table[key].matches(letter + "%"))
+      if letter == "#"
+        where(arel_table[key].matches_regexp("^[0-9]"))
+      else
+        where(arel_table[key].matches(letter + "%"))
+      end
     }
   end
-
 end
--- app/frontend/components/alpha-paginate.vue
+++ app/frontend/components/alpha-paginate.vue
@@ -1,13 +1,13 @@
 <template>
   <nav>
-    <ol class="flex place-content-center mb-10 gap-2">
+    <ol class="flex place-content-center mb-10 gap-1">
       <li
         v-for="letter in letters"
         :key="letter"
       >
         <router-link
           :to="{ query: { letter } }"
-          class="uppercase rounded flex place-content-center place-items-center text-xs w-6 h-6 bg-gray-600"
+          class="uppercase rounded flex place-content-center place-items-center text-xs w-5 h-5 bg-gray-600"
         >
           {{ letter }}
         </router-link>
