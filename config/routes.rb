c9e4cf352bf24d825f3f2680c4f55f79c95959b3
Brandon Keepers <brandon@opensoul.org>
2022-01-12 13:59:20 -0500

    Lint

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ Rails.application.routes.draw do
     resources :songsheets
   end
 
-  mount GoodJob::Engine => 'admin/jobs'
+  mount GoodJob::Engine => "admin/jobs"
 
   get "*anything", to: "content#index"
 end
