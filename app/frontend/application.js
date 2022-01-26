68d78c13cee7b8ca3c4fdf269a6c2c6c90107815
Brandon Keepers <brandon@opensoul.org>
2022-01-26 16:18:02 -0500

    Remove commented out componet code

--- app/frontend/application.js
+++ app/frontend/application.js
@@ -4,23 +4,16 @@ import { createApp } from 'vue'
 import { IonicVue } from '@ionic/vue'
 import App from './views/App.vue'
 import VueAxios from 'vue-axios'
+import client from './client'
 import store from './store'
 import router from './router'
-import api from './client'
-
-// const components = import.meta.globEager('./components/*.vue')
 
 const app = createApp(App)
   .use(IonicVue)
-  .use(VueAxios, api)
+  .use(VueAxios, client)
   .use(store)
   .use(router)
 
 router.isReady().then(() => {
-  // Object.entries(components).forEach(([path, definition]) => {
-  //   const componentName = path.split('/').pop().replace(/\.vue$/, '')
-  //   app.component(componentName, definition.default)
-  // })
-
   app.mount('[data-behavior=vue]')
 })
