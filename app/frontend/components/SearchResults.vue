5076a0c6ee0ce0839ffa8186550022af04b559f7
Brandon Keepers <brandon@opensoul.org>
2022-01-26 09:19:52 -0500

    Get search working with ionic

---
+++ app/frontend/components/SearchResults.vue
@@ -0,0 +1,56 @@
+<template>
+  <ion-list>
+    <ion-item
+      v-for="result in results"
+      :key="result.type + result.id"
+      :router-link="{ name: result.type.toLowerCase(), params: { id: result.id } }"
+    >
+      <ion-avatar slot="start">
+        <img :src="result.thumbnail"> <!-- class="rounded" -->
+      </ion-avatar>
+      <ion-label>
+        <p class="uppercase">
+          {{ result.type }}
+        </p>
+        <h2>{{ result.title }}</h2>
+        <p>{{ result.subtitle }}</p>
+      </ion-label>
+    </ion-item>
+  </ion-list>
+</template>
+
+<script>
+import { IonList, IonItem, IonAvatar, IonLabel } from '@ionic/vue'
+
+export default {
+  components: { IonList, IonItem, IonAvatar, IonLabel },
+
+  props: {
+    params: {
+      type: Object,
+      required: true
+    }
+  },
+
+  data () {
+    return {
+      results: []
+    }
+  },
+
+  watch: {
+    params: { immediate: true, deep: true, handler: 'update' }
+  },
+
+  methods: {
+    async update () {
+      if (!this.params.q) {
+        this.results = []
+        return
+      }
+
+      this.results = (await this.$http.get('/api/search', { params: this.params })).data
+    }
+  }
+}
+</script>
--- app/frontend/views/DiscoverView.vue
+++ app/frontend/views/DiscoverView.vue
@@ -12,29 +12,94 @@
             Discover
           </ion-title>
         </ion-toolbar>
+        <ion-toolbar>
+          <ion-searchbar
+            ref="input"
+            v-model="searchParams.q"
+            :show-cancel-button="isSearching ? 'always' : 'focus'"
+            debounce="250"
+            animated
+          />
+        </ion-toolbar>
+        <ion-toolbar v-show="isSearching">
+          <ion-segment
+            :value="searchParams.type || ''"
+            @ion-change="searchParams.type = $event.detail.value"
+          >
+            <ion-segment-button
+              v-for="id, name in types"
+              :key="id"
+              :value="id"
+            >
+              <ion-label>{{ name }}</ion-label>
+            </ion-segment-button>
+          </ion-segment>
+        </ion-toolbar>
       </ion-header>
 
-      <!-- Default Searchbar -->
-      <ion-searchbar
-        v-model="q"
-        debounce="200"
+      <search-results
+        v-show="isSearching"
+        :params="searchParams"
       />
-
-      <GenreListView />
+      <GenreListView v-show="!isSearching" />
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonSearchbar } from '@ionic/vue'
-import { ref } from 'vue'
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonSearchbar, IonSegment, IonSegmentButton, IonLabel } from '@ionic/vue'
+import SearchResults from '@/components/SearchResults.vue'
 import GenreListView from '@/views/GenreListView.vue'
 
 export default {
-  components: { GenreListView, IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonSearchbar },
+  components: { SearchResults, GenreListView, IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonSearchbar, IonSegment, IonSegmentButton, IonLabel },
 
-  setup () {
-    return { q: ref('') }
+  data () {
+    return {
+      searchParams: {},
+      types: {
+        All: '',
+        Artists: 'artist',
+        Albums: 'album',
+        Songs: 'track,songsheet'
+      }
+    }
+  },
+
+  computed: {
+    isSearching () {
+      return !!this.searchParams.q
+    }
+  },
+
+  watch: {
+    $route: { immediate: true, handler: 'updateFromRoute' },
+    searchParams: { deep: true, handler: 'updateRoute' }
+  },
+
+  mounted () {
+    this.focus()
+  },
+
+  methods: {
+    updateFromRoute () {
+      this.searchParams = this.$route.query
+    },
+
+    clear () {
+      this.searchParams = {}
+    },
+
+    updateRoute () {
+      this.$router.push({
+        query: Object.assign({}, this.$route.query, this.searchParams),
+        replace: true
+      })
+    },
+
+    focus () {
+      this.$refs.input.$el.setFocus()
+    }
   }
 }
 </script>
