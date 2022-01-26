f2ad50763e6f7e3865af3161d3ef671f9874af2b
Brandon Keepers <brandon@opensoul.org>
2022-01-26 09:20:09 -0500

    Remove tailwind/forms and tailwind/typography

--- app/frontend/tailwind.config.js
+++ app/frontend/tailwind.config.js
@@ -21,8 +21,6 @@ module.exports = {
     }
   },
   plugins: [
-    require('@tailwindcss/line-clamp'),
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography')
+    require('@tailwindcss/line-clamp')
   ]
 }
