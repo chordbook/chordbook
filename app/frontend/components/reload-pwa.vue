aec6511431d13a152443853b0435f38116146221
Brandon Keepers <brandon@opensoul.org>
2022-02-07 10:28:45 -0500

    Update pwa prompt to use ion-toast

--- app/frontend/components/reload-pwa.vue
+++ app/frontend/components/reload-pwa.vue
@@ -1,44 +1,25 @@
+<template>
+  <ion-toast
+    :is-open="needRefresh"
+    message="Updated content is available."
+    position="top"
+    :buttons="buttons"
+    animated
+  />
+</template>
+
 <script setup>
 import { useRegisterSW } from 'virtual:pwa-register/vue'
+import { IonToast } from '@ionic/vue'
 
-const {
-  offlineReady,
-  needRefresh,
-  updateServiceWorker
-} = useRegisterSW()
+const { needRefresh, updateServiceWorker } = useRegisterSW()
 
-const close = async () => {
-  offlineReady.value = false
+function close () {
   needRefresh.value = false
 }
-</script>
 
-<template>
-  <div
-    v-if="offlineReady || needRefresh"
-    role="alert"
-    class="flex flex-wrap md:flex-nowrap bg-blue-800 text-white text-sm px-6 py-2 justify-between align-middle"
-  >
-    <div class="message mt-1">
-      <span v-if="offlineReady"> App ready to work offline</span>
-      <span v-else>
-        New content available, click on reload button to update.
-      </span>
-    </div>
-    <div class="buttons flex align-middle mt-2 md:mt-0">
-      <button
-        v-if="needRefresh"
-        class="btn btn-small btn-muted mr-4"
-        @click="updateServiceWorker()"
-      >
-        Reload
-      </button>
-      <button
-        class="btn btn-small btn-muted"
-        @click="close"
-      >
-        Close
-      </button>
-    </div>
-  </div>
-</template>
+const buttons = [
+  { text: 'Close', role: 'cancel', handler: close },
+  { text: 'Update', handler: updateServiceWorker }
+]
+</script>
