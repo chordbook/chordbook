cd963a688dcbab5dcecef61f630da0a05f6c992e
Brandon Keepers <brandon@opensoul.org>
2022-02-12 10:03:49 -0500

    strip source before importing

--- Gemfile
+++ Gemfile
@@ -13,9 +13,10 @@ gem "pg_search"
 gem "good_job"
 gem "rack-cors"
 gem "nokogiri"
-gem "chordpro"
+gem "chordpro", git: "https://github.com/bkeepers/chordpro.git", branch: "carriage-returns"
 gem "kaminari"
 gem "addressable"
+gem "charlock_holmes"
 
 group :development, :test do
   gem "byebug", platforms: [:mri, :mingw, :x64_mingw]
--- app/jobs/import/chordpro.rb
+++ app/jobs/import/chordpro.rb
@@ -3,7 +3,7 @@ require "chordpro"
 # Import a songsheet in chordpro format
 class Import::Chordpro < ApplicationJob
   def perform(source, songsheet: nil, **attrs)
-    chordpro = Chordpro.parse(source)
+    chordpro = Chordpro.parse(source.strip)
 
     (songsheet || Songsheet.new).tap do |s|
       s.assign_attributes attrs.merge(source: source, metadata: chordpro.metadata.to_h)
