ee177fca5596b509bebc374aed23ba2b55fd1cab
Brandon Keepers <brandon@opensoul.org>
2022-02-12 11:33:02 -0500

    Prompt to install app

--- client/src/components/ReloadPwa.vue
+++ client/src/components/ReloadPwa.vue
@@ -7,6 +7,7 @@ import { offline } from '@/icons'
 const localPwa = localStorage.getItem('pwa')
 const pwaEnabled = (import.meta.env.APP_PWA || localPwa) && localPwa !== 'false'
 const offlineReady = ref(false)
+const installPrompt = ref(null)
 
 if (pwaEnabled) {
   console.info('PWA enabled, registering service worker...')
@@ -14,8 +15,26 @@ if (pwaEnabled) {
     immediate: true,
     onOfflineReady () { offlineReady.value = true }
   })
+
+  // Save prompt for installing to home screen
+  // https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Add_to_home_screen
+  window.addEventListener('beforeinstallprompt', (e) => {
+    // Stash the event so it can be triggered later.
+    installPrompt.value = e
+    console.debug('PWA: saved install prompt')
+  })
 } else {
   console.info('PWA disabled')
+}
+
+function addToHomeScreen (e) {
+  // Show the prompt
+  installPrompt.value.prompt()
+  // Wait for the user to respond to the prompt
+  installPrompt.value.userChoice.then((choiceResult) => {
+    console.log('Response to A2HS prompt', choiceResult)
+    installPrompt.value = null
+  })
 }
 </script>
 
@@ -25,6 +44,16 @@ if (pwaEnabled) {
     message="Ready to work offline."
     duration="3000"
     :buttons="[{ text: 'Ok', role: 'cancel' }]"
+  />
+  <ion-toast
+    :is-open="installPrompt"
+    message="Add to home screen?"
+    position="top"
+    :buttons="[
+      { text: 'Not Now', role: 'cancel' },
+      { text: 'Add', handler: addToHomeScreen }
+    ]"
+    :duration="5000"
   />
   <UseOnline v-slot="{ isOnline }">
     <ion-toast
