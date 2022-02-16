ec1ceba24aaa805aeaa478e99d37bdc10e7826ab
Brandon Keepers <brandon@opensoul.org>
2022-02-15 20:23:33 -0500

    Don't retry jobs failing for Deserialization

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -3,7 +3,7 @@ class ApplicationJob < ActiveJob::Base
   # retry_on ActiveRecord::Deadlocked
 
   # Most jobs are safe to ignore if the underlying records are no longer available
-  # discard_on ActiveJob::DeserializationError
+  discard_on ActiveJob::DeserializationError
 
   retry_on StandardError, wait: :exponentially_longer, attempts: 10
 end
