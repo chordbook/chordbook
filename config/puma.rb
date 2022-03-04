5c0ca9915091b26136ba490f87cc7b4aea9e5e51
Brandon Keepers <brandon@opensoul.org>
2022-03-04 11:00:13 -0500

    Update config for render

--- config/puma.rb
+++ config/puma.rb
@@ -29,15 +29,17 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # the concurrency of the application would be max `threads` * `workers`.
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
-#
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
-# Use the `preload_app!` method when specifying a `workers` number.
-# This directive tells Puma to first boot the application and load code
-# before forking the application. This takes advantage of Copy On Write
-# process behavior so workers use less memory.
-#
-# preload_app!
+if ENV["WEB_CONCURRENCY"]
+  workers ENV.fetch("WEB_CONCURRENCY")
+
+  # Use the `preload_app!` method when specifying a `workers` number.
+  # This directive tells Puma to first boot the application and load code
+  # before forking the application. This takes advantage of Copy On Write
+  # process behavior so workers use less memory.
+
+  preload_app!
+end
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
