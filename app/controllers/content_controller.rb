c7fa6577a57cdef942fe2dbbb0ddefd76ccc541d
Brandon Keepers <brandon@opensoul.org>
2022-05-18 22:27:37 -0400

    Remove routes for static content

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -1,12 +1,4 @@
 class ContentController < ActionController::Base
-  def index
-    render file: Rails.root.join("public/index.html")
-  end
-
-  def offline
-    render format: :json
-  end
-
   def ping
     # Execute empty query to ensure database access
     ActiveRecord::Base.connection.execute("")
