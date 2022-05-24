775e8b1a6a931f91ba4d958c31d9e35d7f46e55e
Brandon Keepers <brandon@opensoul.org>
2022-05-23 23:14:22 -0400

    Queries to update artists/albums

--- app/jobs/update_popular_counts.rb
+++ app/jobs/update_popular_counts.rb
@@ -1,15 +1,36 @@
 class UpdatePopularCounts < ApplicationJob
   def perform
+    ApplicationRecord.connection.execute <<~EOQ
+      UPDATE artists
+      SET listeners = count.listeners, updated_at = NOW()
+      FROM (
+        SELECT artists.id, sum(tracks.listeners) as listeners
+        FROM artists
+        LEFT JOIN tracks
+        ON tracks.artist_id = artists.id
+        WHERE tracks.songsheets_count > 0
+        GROUP BY artists.id
+      ) AS count
+      WHERE artists.id = count.id
+    EOQ
+
+    ApplicationRecord.connection.execute <<~EOQ
+      UPDATE albums
+      SET listeners = count.listeners, updated_at = NOW()
+      FROM (
+        SELECT albums.id, sum(tracks.listeners) as listeners
+        FROM albums
+        LEFT JOIN tracks
+        ON tracks.album_id = albums.id
+        WHERE tracks.songsheets_count > 0
+        GROUP BY albums.id
+      ) AS count
+      WHERE albums.id = count.id
+    EOQ
+
     # These fields aren't currently indexed
     Searchkick.callbacks(false) do
-      Artist.find_each do |artist|
-        artist.update listeners: artist.tracks.with_songsheet.sum(:listeners) || 0
-      end
-
-      Album.find_each do |album|
-        album.update listeners: album.tracks.with_songsheet.sum(:listeners) || 0
-      end
-
+      # There aren't as many genres as artists/albums, so just iterate over them to udpate
       Genre.find_each do |genre|
         genre.update(
           thumbnail: genre.artists.first&.thumbnail,
