6e7b8fcf0c2494d2fd40dcb6aed0f7751c8b8a23
Brandon Keepers <brandon@opensoul.org>
2022-05-25 09:41:05 -0400

    require authentication to add to library

--- app/frontend/components/AddToLibraryButton.vue
+++ app/frontend/components/AddToLibraryButton.vue
@@ -1,7 +1,9 @@
 <script setup>
-import { defineProps, ref, unref } from 'vue'
+import { defineProps, ref, unref, watch } from 'vue'
 import { useFetch } from '@/client'
+import useAuthStore from '@/stores/auth'
 import { IonButton, IonIcon, toastController } from '@ionic/vue'
+import AuthRequired from '@/components/AuthRequired.vue'
 import * as icons from '@/icons'
 
 const props = defineProps({
@@ -11,11 +13,19 @@ const props = defineProps({
   }
 })
 
+const auth = useAuthStore()
 const exists = ref(false)
 
-useFetch('library', { params: props }).then(({ statusCode }) => {
-  exists.value = unref(statusCode) === 200
-})
+watch(
+  () => auth.isAuthenticated,
+  () => {
+    if (!auth.isAuthenticated) return
+    useFetch('library', { params: props }).then(({ statusCode }) => {
+      exists.value = unref(statusCode) === 200
+    })
+  },
+  { immediate: true }
+)
 
 async function toast (message) {
   const toast = await toastController.create({ message, duration: 3000 })
@@ -38,24 +48,26 @@ function remove () {
 </script>
 
 <template>
-  <ion-button
-    v-if="exists"
-    color="success"
-    @click="remove"
-  >
-    <ion-icon
-      slot="icon-only"
-      :icon="icons.saved"
-    />
-  </ion-button>
+  <auth-required>
+    <ion-button
+      v-if="exists"
+      color="success"
+      @click="remove"
+    >
+      <ion-icon
+        slot="icon-only"
+        :icon="icons.saved"
+      />
+    </ion-button>
 
-  <ion-button
-    v-else
-    @click="add"
-  >
-    <ion-icon
-      slot="icon-only"
-      :icon="icons.save"
-    />
-  </ion-button>
+    <ion-button
+      v-else
+      @click="add"
+    >
+      <ion-icon
+        slot="icon-only"
+        :icon="icons.save"
+      />
+    </ion-button>
+  </auth-required>
 </template>
---
+++ app/frontend/components/AuthRequired.vue
@@ -0,0 +1,20 @@
+<script setup>
+import useAuthStore from '@/stores/auth'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const auth = useAuthStore()
+
+function ensureAuthenticated (event) {
+  if (auth.isAuthenticated) return
+  event.stopPropagation()
+  event.preventDefault()
+  router.push('#signup')
+}
+</script>
+
+<template>
+  <div @click.capture="ensureAuthenticated">
+    <slot />
+  </div>
+</template>
