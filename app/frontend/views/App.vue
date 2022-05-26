14282edca26e07b8c919e9de30e27a5208989a3f
Brandon Keepers <brandon@opensoul.org>
2022-05-25 23:28:08 -0400

    Keep signin/signup modal around after authenticating so it dismisses properly

--- app/frontend/views/App.vue
+++ app/frontend/views/App.vue
@@ -8,44 +8,21 @@
       <app-menu content-id="main" />
       <ion-router-outlet id="main" />
     </ion-split-pane>
-    <user-signin-modal v-if="!auth.isAuthenticated" />
-    <user-signup-modal v-if="!auth.isAuthenticated" />
+    <user-signin-modal />
+    <user-signup-modal />
   </ion-app>
 </template>
 
-<script>
+<script setup>
 import { IonApp, IonRouterOutlet, IonSplitPane } from '@ionic/vue'
 import ReloadPwa from '@/components/reload-pwa.vue'
 import AppMenu from '@/components/menu.vue'
-import { defineComponent } from 'vue'
-import useAuthStore from '@/stores/auth'
 import UserSigninModal from './UserSigninModal.vue'
 import UserSignupModal from './UserSignupModal.vue'
+import useAuthStore from '@/stores/auth'
 
-export default defineComponent({
-  name: 'App',
-
-  components: {
-    IonApp,
-    ReloadPwa,
-    IonSplitPane,
-    AppMenu,
-    IonRouterOutlet,
-    UserSigninModal,
-    UserSignupModal
-  },
-
-  setup () {
-    const auth = useAuthStore()
-    auth.refresh()
-
-    return { auth }
-  },
-
-  errorCaptured (error, instance, info) {
-    window.captureError && window.captureError(error)
-  }
-})
+const auth = useAuthStore()
+auth.refresh()
 </script>
 
 <style scoped>
