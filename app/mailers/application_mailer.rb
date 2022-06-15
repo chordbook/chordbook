0c60f20aa0ac8274a0082e9d8372bc466a2adda4
Brandon Keepers <brandon@opensoul.org>
2022-06-15 13:24:12 -0400

    Update news email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
   SUPPORT_FROM = ENV.fetch("MAIL_FROM", "Chord Book <help@chordbook.app>")
-  NEWS_FROM = ENV.fetch("MAIL_FROM", "Chord Book <news@chordbook.app>")
+  NEWS_FROM = ENV.fetch("MAIL_FROM", "Chord Book <whatsnew@chordbook.app>")
 
   layout "mailer"
 end
