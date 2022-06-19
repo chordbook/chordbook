62f482d5257e3d2abf99ebf9c289c75a35407edc
Brandon Keepers <brandon@opensoul.org>
2022-06-19 09:36:52 -0400

    Remove unused scopes

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,12 +1,9 @@
 class Genre < ApplicationRecord
   has_many :artists, -> { Artist.order_by_popular }
-  has_many :tracks, -> { Track.with_songsheet.order_by_popular }
+  has_many :tracks, -> { Track.order_by_popular }
   has_many :albums, -> { Album.order_by_popular }
 
   has_paper_trail
-
-  has_one :example_track, -> { Track.order_by_popular }, class_name: "Track"
-  has_one :example_artist, through: :example_track, class_name: "Artist", source: :artist
 
   scope :order_by_popular, -> { order("genres.rank") }
 end
--- test/models/genre_test.rb
+++ test/models/genre_test.rb
@@ -1,15 +1,4 @@
 require "test_helper"
 
 class GenreTest < ActiveSupport::TestCase
-  test "example_artist" do
-    genre = create :genre
-    artist = create :artist
-    track = create :track, artist: artist, genre: genre, listeners: 999
-
-    # less popular track
-    create :track, artist: artist, genre: genre, listeners: 1
-
-    assert_equal track, genre.example_track
-    assert_equal artist, genre.example_artist
-  end
 end
