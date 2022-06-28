82137dea44f9ae9d1e2b31feedcb91a81273abcf
Jahtomini Ogunderu <93228906+jahtomini@users.noreply.github.com>
2022-06-29 00:21:46 +0100

    Add collapse functionality for menu

--- app/frontend/components/AppMenu.vue
+++ app/frontend/components/AppMenu.vue
@@ -1,26 +1,32 @@
 <template>
-  <ion-menu class="dark">
-    <ion-header>
+  <ion-menu
+    class="dark mod maxw-minw width-state"
+  >
+    <ion-header class="flex-basic">
       <ion-toolbar>
         <ion-item
           :router-link="{ name: 'home' }"
           lines="none"
           router-direction="root"
-          class="ion-padding-horizontal"
+          class="ion-padding-horizontal ion-item-custom"
         >
           <img
             slot="start"
-            class="inline dark:hidden"
+            class="inline dark:hidden logo"
             :src="icons.logoLight"
             style="height: 20px"
+            image="logo"
           >
           <img
             slot="start"
-            class="hidden dark:inline"
+            class="hidden dark:inline logo"
             :src="icons.logo"
             style="height: 20px"
+            image="logo"
           >
-          <ion-label>
+          <ion-label
+            class="wordmark-label"
+          >
             <img
               alt="Chord Book"
               class="inline dark:hidden"
@@ -34,11 +40,29 @@
               style="height: 14px"
             >
           </ion-label>
+          <ion-button
+            class="hamburger-button"
+            size="small"
+            shape="round"
+            fill="clear"
+            @click="truncate"
+          >
+            <img
+              alt=""
+              class="inline dark:hidden hamburger-icon"
+              src="../assets/hamburger-light.svg"
+            >
+            <img
+              alt=""
+              class="hidden dark:inline hamburger-icon"
+              :src="icons.hamburger"
+            >
+          </ion-button>
         </ion-item>
       </ion-toolbar>
     </ion-header>
 
-    <ion-content class="ion-padding-horizontal">
+    <ion-content class="ion-padding-horizontal width-only">
       <ion-list lines="none">
         <ion-menu-toggle auto-hide="false">
           <ion-item
@@ -50,11 +74,21 @@
             :color="colorFor('home')"
           >
             <ion-icon
+              v-if="untruncated"
               slot="start"
               size="small"
               :icon="icons.home"
             />
-            <ion-label>Home</ion-label>
+            <ion-icon
+              v-else
+              :icon="icons.home"
+              size="small"
+              class="truncated-icon"
+            />
+            <ion-label v-if="untruncated">
+              Home
+            </ion-label>
+            <ion-label v-else />
           </ion-item>
         </ion-menu-toggle>
         <ion-menu-toggle auto-hide="false">
@@ -67,21 +101,44 @@
             :color="colorFor('discover')"
           >
             <ion-icon
+              v-if="untruncated"
               slot="start"
               size="small"
               :icon="icons.search"
             />
-            <ion-label>Discover</ion-label>
+            <ion-icon
+              v-else
+              :icon="icons.search"
+              size="small"
+              class="truncated-icon"
+            />
+            <ion-label v-if="untruncated">
+              Discover
+            </ion-label>
+            <ion-label v-else />
           </ion-item>
         </ion-menu-toggle>
       </ion-list>
 
       <ion-list lines="none">
-        <ion-list-header lines="none">
+        <ion-list-header
+          v-if="untruncated"
+          style="fontSize: 14px"
+          lines="none"
+        >
+          Library
+        </ion-list-header>
+        <ion-list-header
+          v-else
+          style="fontSize: 10px"
+          lines="none"
+          class="truncated-text"
+        >
           Library
         </ion-list-header>
         <ion-menu-toggle auto-hide="false">
           <ion-item
+            v-if="untruncated"
             button
             shape="round"
             router-link="/songsheets"
@@ -96,9 +153,25 @@
             />
             Songs
           </ion-item>
+          <ion-item
+            v-else
+            button
+            shape="round"
+            router-link="/songsheets"
+            router-direction="root"
+            :detail="false"
+            :color="colorFor('songs')"
+          >
+            <ion-icon
+              :icon="icons.song"
+              size="small"
+              class="truncated-icon"
+            />
+          </ion-item>
         </ion-menu-toggle>
         <ion-menu-toggle auto-hide="false">
           <ion-item
+            v-if="untruncated"
             button
             shape="round"
             router-link="/artists"
@@ -113,9 +186,25 @@
             />
             Artists
           </ion-item>
+          <ion-item
+            v-else
+            button
+            shape="round"
+            router-link="/artists"
+            router-direction="root"
+            :detail="false"
+            :color="colorFor('artists')"
+          >
+            <ion-icon
+              :icon="icons.artist"
+              size="small"
+              class="truncated-icon"
+            />
+          </ion-item>
         </ion-menu-toggle>
         <ion-menu-toggle auto-hide="false">
           <ion-item
+            v-if="untruncated"
             button
             shape="round"
             router-link="/albums"
@@ -130,9 +219,25 @@
             />
             Albums
           </ion-item>
+          <ion-item
+            v-else
+            button
+            shape="round"
+            router-link="/albums"
+            router-direction="root"
+            :detail="false"
+            :color="colorFor('albums')"
+          >
+            <ion-icon
+              :icon="icons.album"
+              size="small"
+              class="truncated-icon"
+            />
+          </ion-item>
         </ion-menu-toggle>
         <ion-menu-toggle auto-hide="false">
           <ion-item
+            v-if="untruncated"
             button
             shape="round"
             router-link="/setlists"
@@ -146,6 +251,21 @@
               :icon="icons.setlist"
             />
             Setlists
+          </ion-item>
+          <ion-item
+            v-else
+            button
+            shape="round"
+            router-link="/setlists"
+            router-direction="root"
+            :detail="false"
+            :color="colorFor('setlists')"
+          >
+            <ion-icon
+              :icon="icons.setlist"
+              size="small"
+              class="truncated-icon"
+            />
           </ion-item>
         </ion-menu-toggle>
       </ion-list>
@@ -172,14 +292,93 @@ watch(route, () => (selected.value = route.meta?.selected || selected.value), { 
 function colorFor (item) {
   return item === selected.value ? 'primary' : ''
 }
+
 </script>
 
+<script>
+export default {
+  data () {
+    return {
+      untruncated: true,
+      menuWidth: '330px',
+      hamburgerIconHeight: '14px',
+      rMargin: '32px',
+      wordmarkOpacity: '1'
+    }
+  },
+  methods: {
+    truncate (e) {
+      e.preventDefault()
+      if (this.untruncated) {
+        this.untruncated = false
+        this.menuWidth = '100px'
+        this.rMargin = '10px'
+        this.wordmarkOpacity = '0'
+      } else {
+        this.untruncated = true
+        this.menuWidth = '330px'
+        this.hamburgerIconHeight = '16px'
+        this.rMargin = '32px'
+        this.wordmarkOpacity = '1'
+      }
+    }
+  }
+}
+</script>
 <style scoped>
+* {
+  transition: 0.4s ease-in-out;
+}
+
+.logo {
+  margin-right: v-bind('rMargin');
+}
+
 ion-menu {
   @apply border-r dark:border-zinc-900;
 }
 
 .ios ion-item {
   @apply rounded-lg;
+}
+
+.ion-item-custom::part(native){
+  --padding-end: 5px;
+}
+
+.flex-basic {
+  display: flex;
+}
+
+.maxw-minw {
+  max-width: 500px;
+  min-width: 130px;
+}
+.item-inner {
+  padding-right: 0px;
+}
+.width-state {
+  max-width: v-bind('menuWidth');
+}
+
+.wordmark-label {
+  opacity: v-bind(wordmarkOpacity);
+}
+
+.hamburger-button {
+  --background: none;
+  --padding-end: 5px;
+  --padding-start: 5px;
+  width: fit-content;
+  margin-right: 0px;
+  margin-left: auto;
+}
+
+.hamburger-icon {
+  height: v-bind('hamburgerIconHeight');
+}
+
+.truncated-icon, .truncated-text {
+  margin-left: 20px;
 }
 </style>
