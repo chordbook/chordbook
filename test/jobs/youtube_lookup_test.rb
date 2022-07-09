608e57aebc2bbba6d6c1ba20c9755bad1e22f4be
Brandon Keepers <brandon@opensoul.org>
2022-07-09 12:25:44 -0400

    run job directly to see if an error is being swallowed

--- test/jobs/youtube_lookup_test.rb
+++ test/jobs/youtube_lookup_test.rb
@@ -4,7 +4,7 @@ class YoutubeLookupTest < ActiveJob::TestCase
   test "finds youtube videos for track" do
     track = create :track, title: "Banana Pancakes", artist: create(:artist, name: "Jack Johnson")
     VCR.use_cassette("youtube/banana_pancakes") do
-      YoutubeLookup.perform_now track
+      YoutubeLookup.new.perform track
 
       assert_equal 2, track.reload.media.size
     end
