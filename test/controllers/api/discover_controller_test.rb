4685659989580692b1d892c511a6bec6f10d1bbb
Brandon Keepers <brandon@opensoul.org>
2022-07-10 10:24:09 -0400

    Remove useless assignment

--- test/controllers/api/discover_controller_test.rb
+++ test/controllers/api/discover_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class Api::DiscoverControllerTest < ActionDispatch::IntegrationTest
   test "includes top setlists for app user" do
-    setlist = create :setlist, user: User.app
+    create :setlist, user: User.app
 
     get api_discover_path(format: :json)
     assert_response :success
