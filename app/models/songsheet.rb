1a94fe9e03e7eede007e987ecc70079c3e70471f
Brandon Keepers <brandon@opensoul.org>
2022-07-12 09:25:46 -0400

    Fix error when deleting songsheet in a setlist

--- app/models/songsheet.rb
+++ app/models/songsheet.rb
@@ -9,6 +9,7 @@ class Songsheet < ApplicationRecord
   has_many :artist_works, as: :work, dependent: :destroy
   has_many :artists, through: :artist_works
   has_many :library_items, as: :item, dependent: :destroy
+  has_many :setlist_items, dependent: :destroy
 
   scope :order_by_popular, -> { order("songsheets.rank") }
   scope :order_by_recent, -> { order(created_at: :desc) }
