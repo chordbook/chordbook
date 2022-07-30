4335b97402d8536a4feb54a132af110f9ac9c66e
Brandon Keepers <brandon@opensoul.org>
2022-07-30 10:38:29 -0400

    Attach images to setlist when updating

--- app/models/setlist.rb
+++ app/models/setlist.rb
@@ -2,19 +2,27 @@ class Setlist < ApplicationRecord
   has_paper_trail
 
   belongs_to :user
-  has_many :items, -> { order(position: :asc) }, class_name: "SetlistItem", dependent: :destroy
+  has_many :items, -> { order(position: :asc) }, class_name: "SetlistItem", dependent: :destroy,
+    after_add: :update_thumbnails, after_remove: :update_thumbnails
   has_many :songsheets, through: :items
   has_many :tracks, through: :songsheets
   has_many :albums, through: :tracks
   has_many :album_images, through: :albums, source: :image_attachment
   has_many :library_items, as: :item, dependent: :destroy
 
+  has_many_attached :thumbnails do |attachable|
+    Album.attachment_reflections["image"].variants.each do |name, options|
+      attachable.variant(name, options)
+    end
+  end
+
   after_create { user.library.add self }
 
   scope :order_by_recent, -> { order(updated_at: :desc) }
   scope :order_by_popular, -> { order("setlists.rank") }
 
-  def thumbnails
-    album_images.select("active_storage_attachments.*, albums.rank").includes(:blob).reorder("albums.rank").distinct.limit(4)
+  def update_thumbnails(_ = nil)
+    update thumbnails: album_images.select("active_storage_attachments.*, albums.rank")
+      .includes(:blob).reorder("albums.rank").distinct.limit(4).map(&:blob)
   end
 end
