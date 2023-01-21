ea3e862b4ea452bf46250a00ec10e34a829f1d84
Brandon Keepers <brandon@opensoul.org>
2023-01-21 08:20:17 -0500

    Fix reference to old name variable

--- client/src/components/SongSheet.vue
+++ client/src/components/SongSheet.vue
@@ -184,7 +184,7 @@ function guessKey (song) {
             class="chord-diagram inline-block"
             xmlns="http://www.w3.org/2000/svg"
             role="image"
-            :title="name"
+            :title="chord.toString({ useUnicodeModifier: true })"
           >
             <use
               :xlink:href="`#chord-${chord}`"
