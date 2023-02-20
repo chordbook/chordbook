2ce0f13ccce423958b842a9340a177d6136182ca
Brandon Keepers <brandon@opensoul.org>
2023-02-20 11:13:47 -0500

    Reduce sentry sample rate

--- client/src/sentry.js
+++ client/src/sentry.js
@@ -20,6 +20,6 @@ export default function (app, router) {
     // Set tracesSampleRate to 1.0 to capture 100%
     // of transactions for performance monitoring.
     // We recommend adjusting this value in production
-    tracesSampleRate: 1.0
+    tracesSampleRate: 0.1
   })
 }
