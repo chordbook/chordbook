e123a980388497268e6e5545032f74dc03fe318e
Brandon Keepers <brandon@opensoul.org>
2024-04-07 09:59:04 -0400

    Add tooltip

--- client/src/components/FontSizeControl.vue
+++ client/src/components/FontSizeControl.vue
@@ -6,13 +6,19 @@ const theme = useThemeStore()
 </script>
 
 <template>
-  <ion-button @click="theme.decreaseFontSize()">
+  <ion-button
+    @click="theme.decreaseFontSize()"
+    v-tooltip="'Decrease font size'"
+  >
     <ion-icon
       :icon="textOutline"
       class="text-base mt-1"
     />
   </ion-button>
-  <ion-button @click="theme.increaseFontSize()">
+  <ion-button
+    @click="theme.increaseFontSize()"
+    v-tooltip="'Increase font size'"
+  >
     <ion-icon :icon="textOutline" />
   </ion-button>
 </template>
