5ef634fc3ced7a77116e6b1da7e5c09ab943d011
Brandon Keepers <brandon@opensoul.org>
2024-04-10 15:45:39 -0400

    Add some docs on useSongsheetParser

--- client/src/composables/useSongsheetParser.js
+++ client/src/composables/useSongsheetParser.js
@@ -4,9 +4,16 @@ import { Chord, ChordLyricsPair } from 'chordsheetjs'
 import { useChords } from '@/composables'
 
 export default function useSongsheetParser (source) {
+  // The parser to use for the source document (ChordPro, ChordsOverWords, UltimateGuitar)
   const parser = computed(() => detectFormat(toValue(source)))
+
+  // The last error that occurred while parsing the source document
   const error = ref()
+
+  // The number of steps to transpose
   const transpose = ref(0)
+
+  // The capo position
   const capo = computed({
     get: () => Number(song.value.capo || 0),
     set: (to) => {
@@ -14,15 +21,23 @@ export default function useSongsheetParser (source) {
       song.value = song.value.transpose(from - to).setCapo(to)
     }
   })
+
+  // The parsed songsheet as a `Song` object from ChordSheetJS
   const song = ref()
+
+  // The chords used in the song
   const chords = useChords(song)
+
+  // The concert key of the song
   const key = computed(() => song.value?.metadata.calculateKeyFromCapo())
 
+  // Transpose the song when the transpose value changes
   watch(transpose, (to, from) => {
     song.value = song.value?.transpose(to - from)
   })
 
-  // Using watchEffect instead of computed so we can keep the previously parsed song if it fails.
+  // Parse the song when the source changes. This uses `watchEffect` instead of `computed` so we
+  // can keep the previously parsed song if it fails.
   watchEffect(() => {
     try {
       song.value = normalize(parser.value?.parse(toValue(source)))
@@ -33,10 +48,10 @@ export default function useSongsheetParser (source) {
     }
   })
 
+  // Set a default key if the song doesn't have one
   watchEffect(() => {
-    if (song.value && !song.value.key) {
-      song.value = song.value.setKey(guessKey(chords.value))
-    }
+    if (!song.value || song.value.key) return
+    song.value = song.value.setKey(guessKey(chords.value))
   })
 
   return { song, chords, error, parser, transpose, capo, key }
