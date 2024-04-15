b9413cf222d4091d717039effbc85dff5f40205a
Brandon Keepers <brandon@opensoul.org>
2024-04-15 07:16:28 -0400

    Show error as alert instead of hiding page

--- client/src/components/AppView.vue
+++ client/src/components/AppView.vue
@@ -1,50 +1,29 @@
 <script setup>
 import { ref, onErrorCaptured, watch } from 'vue'
 import { useOnline } from '@vueuse/core'
+import ErrorMessage from '@/components/ErrorMessage.vue'
+import OfflineMessage from '@/components/OfflineMessage.vue'
 
 const online = useOnline()
 const error = ref(null)
 
 onErrorCaptured((err, instance, info) => {
-  console.log('ERROR', err, instance, info)
   error.value = err
 })
 
 // Clear error to try again when internet connection restored
-watch(online, (online) => {
-  if (online) error.value = null
-})
-
+watch(online, (online) => { if (online) error.value = null })
 </script>
 
 <template>
   <ion-page>
-    <template v-if="error">
-      <ion-header translucent>
-        <ion-toolbar>
-          <ion-buttons slot="start">
-            <ion-back-button
-              text=""
-              default-href="/"
-            />
-          </ion-buttons>
-        </ion-toolbar>
-      </ion-header>
-      <ion-content>
-        <blank-slate
-          v-if="online"
-          icon="warning"
-          title="Something went wrong"
-          description="An unexpected error occurred."
-        />
-        <blank-slate
-          v-else
-          icon="offline"
-          title="Not available offline"
-          description="Try again when you are online and it will be available offline next time."
-        />
-      </ion-content>
-    </template>
+    <error-message
+      v-if="error && online"
+      :error="error"
+      @did-dismiss="error = null"
+    />
+
+    <OfflineMessage v-if="error && !online" />
     <loading v-else>
       <slot />
     </loading>
