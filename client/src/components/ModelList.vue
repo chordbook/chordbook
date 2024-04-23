b17f75a8338f2c789738b26edab579cb4f8d3353
Brandon Keepers <brandon@opensoul.org>
2024-04-23 08:31:18 -0400

    Allow elements to break out of main content padding

--- client/src/components/ModelList.vue
+++ client/src/components/ModelList.vue
@@ -17,7 +17,7 @@ const props = defineProps({
 <template>
   <HorizontalScroller
     :class="{
-      'grid-scroll-x touch:[--auto-cols-peek:0.9]': true,
+      'grid-scroll-x touch:[--auto-cols-peek:0.9] touch:main-content-breakout touch:main-content-padding': true,
       'grid-rows-3 auto-cols-1/1 sm:auto-cols-1/2 lg:auto-cols-1/3 2xl:auto-cols-1/4': props.format === 'item',
       'auto-cols-1/2 sm:auto-cols-1/3 md:auto-cols-1/4 lg:auto-cols-1/5 xl:auto-cols-1/6 2xl:auto-cols-1/8': props.format === 'card'
     }"
