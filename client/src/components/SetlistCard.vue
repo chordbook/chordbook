7800e62e116ee967ad8093819df8283eac866284
Brandon Keepers <brandon@opensoul.org>
2024-05-16 08:28:14 -0500

    Format setlist card

--- client/src/components/SetlistCard.vue
+++ client/src/components/SetlistCard.vue
@@ -22,15 +22,23 @@ defineProps({
 </script>
 
 <template>
-  <ion-item button :router-link="{ name: 'setlist', params: { id } }" class="rounded group @container" :detail="false"
-    lines="none">
+  <ion-item
+    button
+    :router-link="{ name: 'setlist', params: { id } }"
+    class="rounded group @container"
+    :detail="false"
+    lines="none"
+  >
     <ion-label class="me-0 relative aspect-square rounded-lg overflow-hidden">
       <div class="absolute inset-0 w-full h-full">
         <SetlistAvatar :id="id" :thumbnails="thumbnails" />
       </div>
       <div
-        class="relative w-full h-full flex items-end p-4 bg-gradient-to-b from-transparent from-50% to-black/60 to-90%">
-        <div class="text-shadow font-semibold text-white text-balance line-clamp-3">
+        class="relative w-full h-full flex items-end p-4 bg-gradient-to-b from-transparent from-50% to-black/60 to-90%"
+      >
+        <div
+          class="text-shadow font-semibold text-white text-balance line-clamp-3"
+        >
           {{ title }}
         </div>
       </div>
