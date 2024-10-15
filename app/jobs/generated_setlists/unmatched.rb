7ee5521d900f656124e0f5ea2c0442f59e02b298
Brandon Keepers <brandon@opensoul.org>
2024-10-15 17:00:25 -0400

    Exclude traditional songs from "Unmatched" playlist

--- app/jobs/generated_setlists/unmatched.rb
+++ app/jobs/generated_setlists/unmatched.rb
@@ -1,7 +1,11 @@
 class GeneratedSetlists::Unmatched < GeneratedSetlists
+  EXCLUDED = [
+    "Traditional"
+  ]
+
   def perform
     update_setlist "Unmatched" do
-      Songsheet.where(track_id: nil).order_by_recent
+      Songsheet.where(track_id: nil).where("metadata->>'subtitle' NOT IN (?)", EXCLUDED).order_by_recent
     end
   end
 end
