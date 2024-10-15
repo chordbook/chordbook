5a7f3844f8acd854f5ca6a37260576f3f66dfa8f
Brandon Keepers <brandon@opensoul.org>
2024-10-15 11:59:27 -0400

    Remove old mailgun gem, properly configure mailgun

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,4 +1,8 @@
 if ENV["MAILGUN_API_KEY"]
+  Mailgun.configure do |config|
+    config.api_key = ENV["MAILGUN_API_KEY"]
+  end
+
   Rails.application.configure do
     config.action_mailer.delivery_method = :mailgun
     config.action_mailer.mailgun_settings = {
