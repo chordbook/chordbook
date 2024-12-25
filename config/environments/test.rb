11e87c9c4a84c5f1d7e42dc6759400b4c08e52bb
Brandon Keepers <brandon@opensoul.org>
2024-12-25 08:12:27 -0500

    Don't perform caching in test

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -20,6 +20,7 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
+  config.action_controller.perform_caching = false
   config.cache_store = :null_store
 
   # Raise exceptions instead of rendering exception templates.
