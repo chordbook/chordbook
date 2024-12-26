10bc6b0ecbd6514dd84a1783fa70850118a9fab8
Brandon Keepers <brandon@opensoul.org>
2024-12-26 08:05:49 -0500

    Convert util to typescript

--- client/src/util.ts
+++ client/src/util.ts
@@ -1,20 +1,20 @@
 import arrify from "arrify";
 
-export function pluralize(count, singular, plural) {
+export function pluralize(count: number, singular: string, plural: string) {
   return [count, count === 1 ? singular : plural].join(" ");
 }
 
-export function formatArray(args) {
+export function formatArray(args: string | string[]) {
   return new Intl.ListFormat().format(arrify(args));
 }
 
-export function formatDate(input) {
+export function formatDate(input: string) {
   if (!input) return "";
   const date = new Date(input);
   return new Intl.DateTimeFormat(navigator.language).format(date);
 }
 
-export function hostname(url) {
+export function hostname(url: string) {
   try {
     return new URL(url).hostname;
   } catch {
